d9bc1cd5fef563037b0522b5cd192f2a16599eda
JulsCode <jcah549@gmail.com>
2022-02-23 17:41:27 -0600

    agregando detalle del producto

--- src/main/java/com/julscode/springecommerce/controller/HomeController.java
+++ src/main/java/com/julscode/springecommerce/controller/HomeController.java
@@ -1,16 +1,24 @@
 package com.julscode.springecommerce.controller;
 
+import java.util.Optional;
+
+import com.julscode.springecommerce.model.Producto;
 import com.julscode.springecommerce.service.ProductoService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+   private final  Logger log = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private ProductoService productoService;
@@ -20,6 +28,17 @@ public class HomeController {
         model.addAttribute("productos", productoService.findAll());
 
         return "usuario/home";
+    }
+
+    @GetMapping("/productohome/{id}")
+    public String productoHome( @PathVariable Integer id , Model model){
+
+       Producto producto = new Producto();
+       Optional<Producto> productoOptional = productoService.get(id);
+       producto = productoOptional.get();
+       model.addAttribute("producto", producto);
+        return "usuario/productohome";
+
     }
     
 }
