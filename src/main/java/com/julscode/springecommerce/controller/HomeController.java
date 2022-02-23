1256eae78303706054b8a765eb68e23d1920e573
JulsCode <jcah549@gmail.com>
2022-02-23 17:53:46 -0600

    empezando a implementar el carriro de compras

--- src/main/java/com/julscode/springecommerce/controller/HomeController.java
+++ src/main/java/com/julscode/springecommerce/controller/HomeController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -39,6 +40,14 @@ public class HomeController {
        model.addAttribute("producto", producto);
         return "usuario/productohome";
 
+    }
+
+
+    @PostMapping("/cart")
+    public String addCart(){
+
+
+        return "usuario/carrito";
     }
     
 }
