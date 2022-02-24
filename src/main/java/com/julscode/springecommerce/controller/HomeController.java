8529738c58ccebd766a445093d636484aac8676c
JulsCode <jcah549@gmail.com>
2022-02-23 20:19:38 -0600

    se agrego la redireccion a la vista resumenorden

--- src/main/java/com/julscode/springecommerce/controller/HomeController.java
+++ src/main/java/com/julscode/springecommerce/controller/HomeController.java
@@ -121,4 +121,11 @@ public class HomeController {
         return "/usuario/carrito";
     }
 
+    @GetMapping("/order")
+    public String order(){
+
+
+        return "usuario/resumenorden";
+    }
+
 }
